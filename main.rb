bf12922c819a903a8c07d50357b3d5fd82d64515
Uzochukwu Obeki <uobeki.40@gmail.com>
2019-09-07 19:10:39 +0000

    Done.

---
+++ main.rb
@@ -0,0 +1,24 @@
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+# ... The rest of the program
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
